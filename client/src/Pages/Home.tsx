49cdf621704077658e64552022d60c8da31082cf
MatHoyer <mathieuhoyer@gmail.com>
2024-06-14 17:24:10 +0200

    use navigate instead of link

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -2,18 +2,25 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import socket from '@/lib/socket';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const [gameID, setGameID] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col items-center space-y-5">
       <h1 className="text-4xl">Mat Games</h1>
       <Button onClick={() => socket.emit('create-lobby')}>Create Game</Button>
       <Input type="text" onChange={(e) => setGameID(e.target.value)} placeholder="Game ID"></Input>
-      <Button disabled={!gameID} onClick={() => socket.emit('join-lobby', gameID)} asChild>
-        <Link to={'/gameid/' + gameID}>Join Game</Link>
+      <Button
+        disabled={!gameID}
+        onClick={() => {
+          socket.emit('join-lobby', gameID);
+          navigate('/' + gameID);
+        }}
+      >
+        Join Game
       </Button>
     </div>
   );
