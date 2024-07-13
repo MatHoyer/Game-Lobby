29a467cad909e6210d133fd4ee78a434d7948a97
MatHoyer <mathieuhoyer@gmail.com>
2024-07-13 22:03:32 +0200

    end

--- server/src/index.ts
+++ server/src/index.ts
@@ -139,7 +139,6 @@ io.on('connection', (socket) => {
       }
     }
     const players = game.players.filter((p) => p !== player);
-    if (players.length - 1 <= 0) return;
     players[0]?.socket.emit('game-play');
   });
 });
